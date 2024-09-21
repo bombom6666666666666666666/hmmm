8824ea85f27d968d233a792341b0fb8ad59b64e6
bombom6666666666666666666 <boz239797@gmail.com>
2024-09-21 10:51:24 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,6 @@
-import os
 import discord
 from discord.ext import commands
 import asyncio
-
-from myserver import server_on
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -41,6 +38,5 @@ async def verify(ctx):
 
     except asyncio.TimeoutError:
         await ctx.author.send('คุณใช้เวลานานเกินไปในการตอบกลับ! การยืนยันถูกยกเลิก.')
-server_on()
 
-bot.run(os.getenv)('TOKEN')
+bot.run(TOKEN)
